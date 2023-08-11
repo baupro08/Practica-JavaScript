a83da51e457810d336c1268877557d3b2a46ea1d
baupro08 <137501523+baupro08@users.noreply.github.com>
2023-08-11 09:32:52 -0300

    archivos iniciales

---
+++ clase1.js
@@ -0,0 +1,36 @@
+let suma= 3+3 
+console.log("el resultado es" + suma)
+
+let division= 10/2
+console.log("el resultado es" / division)
+
+let multiplicacion= 2*5
+console.log("el resultado es" * multiplicacion)
+
+let resta= 10-5
+console.log("el resultado es" - resta)
+
+alert("el resultado de la suma es" + suma) 
+alert("el resultado de la division es" + division)
+alert("el resultado de la multplicacion es" + multiplicacion)
+alert("el resultado de la resta es" + resta)
+
+let edadalumno= 2023-2008
+alert("mi edad es" + edadalumno)
+
+let edad
+edad = prompt("ingresa tu año de nacimiento")
+
+let nombrealumno
+
+nombrealumno = prompt("ingresa yu nombre")
+
+alert("bienvenido" + nombrealumno)
+
+let nombrealumno= "Bauti"
+let apellidoalumno="Gaya"
+
+console.log("bienvenido" + nombrealumno + apellidoalumno)
+
+
+
